1c0f020f36ae69baee8586d019145d22e32ad61c
Maria Waslick <mariawaslick@gmail.com>
2022-02-14 16:59:14 -0500

    started setting up components and added name

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import './LandingPage.css'
+
+export default function LandingPage() {
+    return (
+        <Box>
+            <Typography variant="h1" component="div" gutterBottom>
+                Maria Waslick
+            </Typography>
+            <Typography variant="h2" gutterBottom component="div">
+                full-stack web development
+            </Typography>
+        </Box>
+    )
+}
